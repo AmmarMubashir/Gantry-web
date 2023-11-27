981be4f9b22bffb60ecd42845a52acbdb7bceb57
Ammar <muhammadammarmubashir@gmail.com>
2023-11-27 10:17:27 +0500

    All set

---
+++ components/hometransition/homeTransition.jsx
@@ -0,0 +1,126 @@
+"use client";
+import React from 'react';
+
+import "../hometransition/hometransition.css";
+
+const HomeTransition = () => {
+
+    return (
+        <div class="railscontainee">
+        <div class="horizontalbar">
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+        </div>
+        <div class="horizontalbar">
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+        </div>
+        <div class="horizontalbar">
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+        </div>
+        <div class="horizontalbar">
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+        </div>
+        <div class="horizontalbar">
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+            <div class="verticlebar"></div>
+        </div>
+    
+    </div>
+
+    );
+};
+
+export default HomeTransition;
